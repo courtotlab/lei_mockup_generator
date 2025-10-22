e193eb7743d80bc3adffa12190aef7aea862eca3
Jochen Weile <jweile@oicr.on.ca>
2025-10-22 13:02:51 -0400

    updated readme, gitignore and dependencies script

---
+++ scripts/install_dependencies.R
@@ -0,0 +1,25 @@
+#!/usr/bin/env Rscript
+
+
+## Assuming this is run in a fresh pixi environment, nothing should
+## Be installed yet, so we don't check for existing packages
+
+repo <- "https://cloud.r-project.org/"
+
+#"normal" packages
+install.packages(
+  c("httr", "argparser", "RJSONIO", "yaml", "hash"),
+  repos = repo
+)
+
+#biomart via bioconductor
+install.packages("BiocManager", repos = repo)
+BiocManager::install("biomaRt", ask = FALSE)
+
+#github packages via "remotes"
+install.packages("remotes", repos = repo)
+remotes::install_github("VariantEffect/hgvsParseR")
+
+#tinytex installer
+install.packages("tinytex", repos = repo)
+tinytex::install_tinytex()
